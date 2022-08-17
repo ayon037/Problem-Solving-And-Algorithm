cdadcb779f3cc4aefc91c841764ebbb435a666d9
ayon <mohiuddin.310799@gmail.com>
2022-08-17 23:03:49 +0600

    Vjudge Contest For ICPC Preliminary Team Making

--- C++.cpp
+++ C++.cpp
@@ -7,33 +7,39 @@ using namespace std;
 #define infinity 1 << 30
 typedef long double Tf;
 const Tf EPS = 1e-9;
+const ll MX = 1e5 + 123;
 
 int main()
 {
     Charpoka;
-    int t;
-    cin >> t;
-    while (t--)
+    int n, f, d;
+    cin >> n >> f >> d;
+    vector<int> v;
+    map<int, vector<int>> mp;
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        cin >> n;
-        map<char, int> mp;
-        int cnt = 0;
-        for (int i = 1; i <= n; i++)
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    int flag = 0;
+    for (int i = 0; i < n; i++)
+    {
+        mp[v[i] % d].push_back(v[i]);
+        if (mp[v[i] % d].size() == f)
         {
-            char ch;
-            cin >> ch;
-            mp[ch]++;
-            if (mp[ch] == 1)
+            flag = 1;
+            cout << "Yes" << endl;
+            for (int j = 0; j < f; j++)
             {
-                cnt += 2;
+                cout << mp[v[i] % d][j] << " ";
             }
-            else
-            {
-                cnt++;
-            }
+            break;
         }
-        cout << cnt << endl;
+    }
+    if (flag == 0)
+    {
+        cout << "No";
     }
     return 0;
 }
