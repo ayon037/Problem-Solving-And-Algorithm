aa6b2280687ab017a7d4e1eed52646388bcd457c
ayon <mohiuddin.310799@gmail.com>
2022-08-20 22:56:08 +0600

    Crossmarket Codeforces Solved

--- C++.cpp
+++ C++.cpp
@@ -9,80 +9,26 @@ typedef long double Tf;
 const Tf EPS = 1e-9;
 const ll MX = 1e5 + 123;
 
-int Prime[2000];
-
-void Find_Prime(int high)
-{
-    for (int i = 2; i <= high; i++)
-    {
-        if (Prime[i] == 0)
-        {
-            for (int j = i * i; j <= high; j += i)
-            {
-                Prime[j] = 1;
-            }
-        }
-    }
-}
-
 int main()
 {
-    int n, k;
-    cin >> n >> k;
-    memset(Prime, 0, sizeof(Prime));
-    Find_Prime(n);
-
-    int cnt = 0, flag = 0;
-    for (int i = 2; i <= n; i++)
+    Charpoka;
+    int t;
+    cin >> t;
+    while (t--)
     {
-        int p = 0, sum = 0;
-        if (Prime[i] == 0)
+        int n, m;
+        cin >> n >> m;
+        int a = max(m, n), b = min(m, n);
+        if (m == 1 && n == 1)
         {
-            // cout << i << " ";
-            for (int j = 2; j <= n; j++)
-            {
-                if (Prime[j] == 0)
-                {
-                    // cout << "(I , J) = "
-                    //      << "(" << i << " , " << j << ")"
-                    //      << " , ";
-                    p++;
-                    sum += j;
-                    // cout << sum << " , " << p << endl;
-                    if (p == 2)
-                    {
-                        // cout << "I = " << i << ", Sum = " << sum + 1 << ", P = " << p << endl;
-                        if (sum + 1 == i)
-                        {
-                            cnt++;
-                            break;
-                        }
-                        else if (sum + 1 > i)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            p = 1;
-                            sum = j;
-                        }
-                    }
-                }
-            }
+            cout << max(n, m) - min(n, m) << endl;
         }
-        if (cnt == k)
+        else
         {
-            flag = 1;
-            break;
+            n = a;
+            m = b;
+            cout << (m - 1) * 2 + 1 + n - 1 << endl;
         }
-    }
-    if (flag == 1)
-    {
-        cout << "YES";
-    }
-    else
-    {
-        cout << "NO";
     }
     return 0;
 }
