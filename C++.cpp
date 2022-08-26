4b98293c1171c439b0adf15457ab1692ec526ada
ayon <mohiuddin.310799@gmail.com>
2022-08-26 17:18:01 +0600

    Appleman and Easy Task Solved

--- C++.cpp
+++ C++.cpp
@@ -12,43 +12,36 @@ const ll MX = 1e5 + 123;
 int main()
 {
     Charpoka;
-    int n, m;
-    cin >> n >> m;
-    int pos = 0;
-    int mx = 0;
-    vector<int> v;
-    for (int i = 1; i <= n; i++)
+    int n;
+    cin >> n;
+    char board[n][n];
+    bool even = true;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> board[i][j];
+    for (int i = 0; i < n; i++)
     {
-        int a;
-        cin >> a;
-        if (m < a)
+        int c = 0;
+        for (int j = 0; j < n; j++)
         {
-            if(a%m==0)
-            {
-                a=a/m;
-            }
-            else
-            {
-                a=(a/m)+1;
-            }
-            //cout<<a<<" = After Ceiling "<<i<<endl;
-            if (a >= mx)
-            {
-                //cout<<a<<" = Hello "<<i<<endl;
-                mx = a;
-                pos = i;
-            }
+            if (i - 1 >= 0 and board[i - 1][j] == 'o')
+                c++;
+            if (i + 1 < n and board[i + 1][j] == 'o')
+                c++;
+            if(j - 1 >= 0 and board[i][j - 1] == 'o')
+                c++;
+            if(j + 1 < n and board[i][j + 1] == 'o')
+                c++;
+            if (c % 2 == 1)
+                even = false;
         }
     }
-    if(pos==0)
-    {
-        cout<<n;
-    }
-    else
-    {
-        cout<<pos;
-    }
 
+    if (even)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
     return 0;
 }
 
