87c7a21df19b7c98ed48397c687b0c1eca83ddcb
ayon037 <mohiuddin.310799@gmail.com>
2023-03-08 18:03:48 +0600

    Codeforces Beta Round, Div-2, Problem-F (Fish) Solved

--- C++.cpp
+++ C++.cpp
@@ -27,54 +27,67 @@ typedef long double Tf;
 const Tf EPS = 1e-9;
 const ll MX = 1e8;
 vector<bool>Prime(MX+1,false);
-int cost[21][21];
-int dp[21][(1<<21)];
-int solve(int i,int mask,int n)
+int n;
+double dp[(1<<20)];
+double prob[20][20];
+
+double pMove(int p_mask,int j,int &n)
 {
-    if(i==n)
+    int k = SetBits(p_mask);
+    ll den = (k*(k-1))/2;
+
+    double moveProb=0;
+
+    for(int fish=0;fish<n;fish++)
     {
-        return 0;
-    }
-    if(dp[i][mask]!=-1)
-    {
-        return dp[i][mask];
-    }
-    int ans=INT_MAX;
-    for(int j=0;j<n;j++)
-    {
-        if(mask&(1<<j))
+        if((1<<fish)&p_mask)
         {
-            ans=min(ans,cost[j][i]+solve(i+1,(mask^(1<<j)),n));
+            moveProb+=prob[fish+1][j+1];
         }
     }
-    return dp[i][mask]=ans;
+    return moveProb/(1.0*den);
 }
+double solve(int mask,int &n)
+{
+    int alive=SetBits(mask);
+    if(alive==n)
+    {
+        return 1.0;
+    }
+    if(dp[mask]> -0.9)
+    {
+        return dp[mask];
+    }
 
+    double ans=0;
+    for(int fish=0;fish<n;fish++)
+    {
+        if(!(mask&(1<<fish)))
+        {
+            int p_mask=mask^(1<<fish);
+            double prev_day=solve(p_mask,n);
+            ans+=prev_day*pMove(p_mask,fish,n);
+        }
+    }
+    return dp[mask]=ans;
+}
 int main()
 {
     Charpoka;
-    int n,m,x,i,j,k,q;
+    // Codeforces Beta Round 16 (Div-02) Problem-F (Fish)
+    cin>>n;
     memset(dp,-1,sizeof(dp));
-    TC
+    for(int i=1;i<=n;i++)
     {
-        cin>>n;
-        for(int i=0;i<n;i++)
+        for(int j=1;j<=n;j++)
         {
-            for(int j=0;j<n;j++)
-            {
-                cin>>cost[i][j];
-            }
+            cin>>prob[i][j];
         }
-        cout<<solve(0,(1<<n)-1,n)<<endl;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<solve((1<<i),n)<<" ";
     }
     return 0;
-}
 
-/*
-1
-4
-9 2 7 8
-6 4 3 7
-5 8 1 8
-7 6 9 4
-*/
+}
